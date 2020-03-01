b032dbf278003cec51a32fe13290db060ed53be4
evgenysmirnov <smirkos2@gmail.com>
2020-02-29 22:53:34 -0800

    slash +

---
+++ Kata 7 /Buildings blocks.js
@@ -0,0 +1,61 @@
+//https://www.codewars.com/users/Evgenii%20Smirnov/completed_solutions
+
+
+/*
+
+public class Block {
+    int width ;
+    int length;
+    int height;
+    int volume;
+    int surfaceArea ;
+
+    public Block(int [] array) {
+    this.width = array[0];
+    this.length = array[1];
+    this.height = array[2];
+    this.volume = width * length * height;
+    this.surfaceArea = 2 * length * height + 2 * length * width + 2 * width * height;
+}
+
+public int getWidth() {
+    return width;
+}
+
+public void setWidth(int width) {
+    this.width = width;
+}
+
+public int getLength() {
+    return length;
+}
+
+public void setLength(int length) {
+    this.length = length;
+}
+
+public int getHeight() {
+    return height;
+}
+
+public void setHeight(int height) {
+    this.height = height;
+}
+
+public int getVolume() {
+    return volume;
+}
+
+public void setVolume(int volume) {
+    this.volume = volume;
+}
+
+public int getSurfaceArea() {
+    return surfaceArea;
+}
+
+public void setSurfaceArea(int surfaceArea) {
+    this.surfaceArea = surfaceArea;
+}
+}
+*/
