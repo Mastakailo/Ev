3f2a43cc89970751c544c4358279a42f8f3be72c
evgenysmirnov <smirkos2@gmail.com>
2020-03-01 21:34:53 -0800

    new kata

---
+++ Kata 7 /Figters.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/577bd8d4ae2807c64b00045b
+
+
+/*public class Kata {
+    public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+    while (fighter1.health > 0 && fighter2.health > 0){
+    fighter1.health-=fighter2.damagePerAttack;
+    fighter2.health-=fighter1.damagePerAttack;
+};
+if (fighter1.health <= 0 && fighter2.health <= 0){
+    return firstAttacker;
+}else if (fighter1.health <= 0){
+    return fighter2.name;
+}else {
+    return fighter1.name;
+}
+}
+}
+*/
